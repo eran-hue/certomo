89f9100b6402bee15a5ff0ee654176466b0a758f
eran <eran@milezero.ai>
2025-12-31 10:45:27 +0200

    add ngrock

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -49,6 +49,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
 app.UseStaticFiles(); // Serve static files (HTML/CSS/JS)
 
 app.UseSerilogRequestLogging();
